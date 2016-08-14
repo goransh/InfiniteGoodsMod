491db325c229c68cb8dfc7bffc0c53b45b40133c
Gøran Solheim Humlestøl <goransh@live.no>
2016-08-14 19:40:12 +0200

    Added null check to BuildingInfo as some other mods seems to set this to null.

--- UnlimitedGoodsMod/GoodsMonitor.cs
+++ UnlimitedGoodsMod/GoodsMonitor.cs
@@ -17,17 +17,22 @@ namespace InfiniteGoodsMod
         public override void OnAfterSimulationTick()
         {
             PrefabAI ai;
-            int amount;
+            BuildingInfo info;
+            int goods;
+
             for (var i = (ushort)(simulationManager.m_currentTickIndex % 1000); i < buildingManager.m_buildings.m_buffer.Length; i += 1000)
             {
-                ai = buildingManager.m_buildings.m_buffer[i].Info.GetAI();
-
-                if (ai == null) continue; 
+                info = buildingManager.m_buildings.m_buffer[i].Info;
+                if (info == null) continue;
+                
+                ai = info.GetAI();
+                if (ai == null) continue;
 
                 if (ai is CommercialBuildingAI)
                 {
-                    amount = 100000;
-                    ((CommercialBuildingAI)ai).ModifyMaterialBuffer(i, ref buildingManager.m_buildings.m_buffer[i], TransferManager.TransferReason.Goods, ref amount);
+                    goods = 100000;
+                    ((CommercialBuildingAI)ai).ModifyMaterialBuffer(i, ref buildingManager.m_buildings.m_buffer[i], TransferManager.TransferReason.Goods, ref goods);
+
                 }
             }
         }
