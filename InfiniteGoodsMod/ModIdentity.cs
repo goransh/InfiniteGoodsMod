cf24f39274415c861b901216a1d322637bf44d7e
Gøran Solheim Humlestøl <goransh@live.no>
2018-03-04 15:21:18 +0100

    Clean up folder structure

--- InfiniteGoodsMod/ModIdentity.cs
+++ InfiniteGoodsMod/ModIdentity.cs
@@ -4,7 +4,7 @@ namespace InfiniteGoodsMod {
     public class ModIdentity : IUserMod {
 
         public const ulong WorkshopId = 725555912;
-        public const string Version = "3.0.beta1";
+        public const string Version = "3.0";
 
         public string Name => "Infinite Goods";
 
